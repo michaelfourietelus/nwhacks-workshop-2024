033ef9255925e09f356929b09b0370dc523f1418
imogenemcdonald <imogene.mcdonald@telus.com>
2024-01-08 09:54:16 -0800

    update db url

--- server/src/createMongo.js
+++ server/src/createMongo.js
@@ -1,6 +1,5 @@
 var MongoClient = require('mongodb').MongoClient;
-var url = 'mongodb+srv://michaelfourie:Hasie03@cluster0.jvbnlvh.mongodb.net/?retryWrites=true&w=majority';
-//UPDATE URL
+var url = 'mongodb://localhost:27017/';
 
 MongoClient.connect(url, function(err, db) {
   if (err) throw err;
--- server/src/routes/sleepRoutes.js
+++ server/src/routes/sleepRoutes.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const {
   getSleeps,
   addSleep,
-  updateSleep,
   deleteSleep,
 } = require("../controllers/sleepController");
 
